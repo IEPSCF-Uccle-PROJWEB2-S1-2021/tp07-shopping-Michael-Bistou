98d33fb8df97d5cc14c28d347cc50f6888e3ff80
Michael-Bistou <mimoaz@hotmail.com>
2020-10-26 15:11:27 +0100

    2nd commit

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let count = 0;
 const renderItems = () => {
 
   if (document.querySelector('input').value === '') {
-    return alert('Wrong value')
+    return alert('Wrong value');
   }
 
   const selectDeleteButton = '.'+deleteButton.className;
@@ -22,9 +22,7 @@ const renderItems = () => {
     evt.stopPropagation();
     document.querySelector(selectLi).remove();
   })
-
   document.querySelector('input').focus();
-
 }
 
 addSelectButton.addEventListener('click', renderItems);
